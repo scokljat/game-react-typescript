5558ffc0f1f3856fb3069a60dbd474fe1beba5ff
scokljat <selma.cokljat@hotmail.com>
2023-07-27 19:01:50 +0200

    Added file for word

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,43 @@
+type HangmanWordProps = {
+  wordToGuess: string;
+  guessedLetters: string[];
+  isLoser?: boolean;
+};
+
+export function HangmanWord({
+  wordToGuess,
+  guessedLetters,
+  isLoser = false,
+}: HangmanWordProps) {
+  console.log(wordToGuess);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.24rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: "10px solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || isLoser
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) || isLoser ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
