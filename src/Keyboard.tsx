941a6f5ed8ab36a5d49c1a474636a3b8e1a23240
scokljat <selma.cokljat@hotmail.com>
2023-07-27 19:00:26 +0200

    Added keyboard with style

---
+++ src/Keyboard.tsx
@@ -0,0 +1,72 @@
+import styles from "./Keyboard.module.css";
+
+type KeyboardProps = {
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+  disabled?: boolean;
+};
+
+const Keys = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit,minmax(75px,1fr))",
+        gap: "0.5rem",
+      }}
+    >
+      {Keys.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            key={key}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }
+            `}
+            disabled={isActive || isInactive || disabled}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
